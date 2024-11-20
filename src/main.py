b7a01a5793adad85ffb34d0bfba768cb5767dfd7
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-11-20 10:35:06 +0200

    Implement file downloading and uploading
    Co-authored-by: HRemonen <henri@remonen.fi>
    Co-authored-by: evahteri <eero.vahteri@helsinki.fi>
    Co-authored-by: rikurauhala <riku@rauhala.io>

--- src/main.py
+++ src/main.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse, FileResponse
+import os
 
 app = FastAPI()
 
+FILE_DIRECTORY = '../storage'
+
+if not os.path.exists(FILE_DIRECTORY):
+    os.makedirs(FILE_DIRECTORY)
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    try:
+        contents = file.file.read()
+        with open(os.path.join(FILE_DIRECTORY, file.filename), 'wb') as f:
+            f.write(contents)
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
+
+@app.get("/get/{file_name}")
+async def get(file_name: str):
+    try:
+        return FileResponse(os.path.join(FILE_DIRECTORY, file_name), filename=file_name)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail='File not found')
+    except Exception:
+        raise HTTPException(status_code=500, detail='Something went wrong')
+
+    
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    content = """
+<body>
+<form action="/upload/" enctype="multipart/form-data" method="post">
+<input name="file" type="file">
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
