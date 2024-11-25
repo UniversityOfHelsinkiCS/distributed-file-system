d465d9d60fb2ab83688b29676263c25aa816273f
tfhuhtal <tuomas.huhtala@icloud.com>
2024-11-25 15:26:12 +0200

    redis tunkkaus

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,7 +1,7 @@
 import os
 import redis.asyncio as redis
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST = os.getenv("REDIS_HOST", "http://redis-distributed-filesystem-svc")
 REDIS_PORT = os.getenv("REDIS_PORT", "6379")
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "password")
 
